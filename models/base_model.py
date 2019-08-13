dff503c5bb90a31d7d6cd4eafcabe6df69228fae
snehadasa <sneha.dasasneha@gmail.com>
2019-08-13 20:52:31 +0000

    update all

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,21 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import DateTime
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -26,10 +35,12 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+                if id not in kwargs.keys():
+                    self.id = str(uuid.uid4())
+                    self.created_at = self.updated_at = datetime.now()
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid.uid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -48,6 +59,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -59,4 +71,12 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        if "_sa_instance_state" in my_dict:
+            del my_dict["_sa_instance_state"]
         return my_dict
+
+    def delete(self):
+        """delete the current instance from the storage (models.storage)
+        by calling the method delete
+        """
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+import MySQLdb
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
-       Attributes:
-    name: input name
+    Attributes:
+        name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+
+    if getenv(HBNB_TYPE_STORAGE) = "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref='state')
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            for value in models.storage.all(City).values():
+                if value.state_id == self.id:
+                    return value
