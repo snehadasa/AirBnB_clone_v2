1b08a886d8b8f6e946a4840268e985a3ad9782f6
VieetBubbles <537@holbertonschool.com>
2019-08-13 21:10:51 +0000

    Fixed the uuid4 syntax typo

--- models/base_model.py
+++ models/base_model.py
@@ -36,10 +36,10 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
                 if id not in kwargs.keys():
-                    self.id = str(uuid.uid4())
+                    self.id = str(uuid.uuid4())
                     self.created_at = self.updated_at = datetime.now()
         else:
-            self.id = str(uuid.uid4())
+            self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
