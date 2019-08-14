5177e26302d5ff92fa26031c210b537fec71ef11
VieetBubbles <537@holbertonschool.com>
2019-08-14 23:12:17 +0000

    Send all place_2.py asset to this file

--- models/place.py
+++ models/place.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """This is the place class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Float, ForeignKey, Table, String, DateTime
+import MySQLdb
+from os import getenv
+from models.city import City
+
+"""
+place_amenity = Table('place_amenity', Base.metadata, Column('place_id',
+                      String(60), ForeignKey('places.id'), primary_key=True,
+                      nullable=False),
+                      Column('amenity_id', String(60), ForeignKey(amenities.id),
+                      primary_key=True, nullable=False))"""
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
         city_id: city id
@@ -18,14 +31,48 @@ class Place(BaseModel):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    #amenity_ids = []
+
+    """if getenv("HBNB_TYPE_STORAGE") == 'db':
+        reviews = relationship("Review", backref="place",
+                               cascade='all, delete-orphan')
+        amenities = relationship("Amenity",
+                                 backref='places', viewonly=False)
+
+    elif getenv("HBNB_TYPE_STORAGE") == 'file':
+        @property
+        def reviews(self):
+            new = []
+            for r in self.reviews:
+                if r.place_id == self.id:
+                    new.append(r)
+            return new
+    """
+    """
+        @property
+        def amenities(self):
+            new = []
+            for obj in amenity_ids:
+                if obj.id == self.id:
+                    new.append(obj)
+            return new
+    """
+    """
+        @amenities.setter
+        def amenities(self, obj):
+            if type(obj) == 'Amenity':
+                self.amenity_ids.append(obj.id)
+
+    """
