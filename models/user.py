1cab651cfacd4c3c956e3a94c07432403a893caa
VieetBubbles <537@holbertonschool.com>
2019-08-14 22:11:32 +0000

    commented out the review and place statements

--- models/user.py
+++ models/user.py
@@ -15,10 +15,11 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __tablename__ == 'users'
+    __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="user")
-    reviews = relationship("Review", backref="user")
+    #places = relationship("Place", backref="user")
+    #reviews = relationship("Review", backref="user")
