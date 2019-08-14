44cbbf06b87ca1b678bf0753290227233c4ab2d6
snehadasa <sneha.dasasneha@gmail.com>
2019-08-14 14:46:53 +0000

    city

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from os import getenv
 
 
@@ -14,6 +14,13 @@ class City(BaseModel, Base):
     """
     __tablename__ = 'cities'
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities")
+    name = Column(String(128),
+                  nullable=False)
+
+    state_id = Column(String(60),
+                      ForeignKey('states.id'),
+                      nullable=False)
+
+    #places = relationship("Place", 
+                           #backref="cities",
+                           #cascade="all, delete-orphan")
