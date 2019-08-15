0c272956fc4a15566377c329a19d2910fed5a842
VieetBubbles <537@holbertonschool.com>
2019-08-15 00:41:55 +0000

    Fixed typo in foreignKey(users.id)

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ from sqlalchemy import Float, ForeignKey, Table, String, DateTime
 import MySQLdb
 from os import getenv
 from models.city import City
-from models.user import User
+
 
 """
 place_amenity = Table('place_amenity', Base.metadata, Column('place_id',
@@ -35,7 +35,7 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
