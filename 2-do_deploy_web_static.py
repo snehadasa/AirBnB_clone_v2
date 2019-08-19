b3ee46892224cc5a3e4f1efc81d59da3f36d7242
snehadasa <sneha.dasasneha@gmail.com>
2019-08-19 19:35:02 +0000

    update 0

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-# Fabric script (based on the file 1-pack_web_static.py) that distributes an
-#  archive to your web servers, using the function do_deploy
+""" Fabric script (based on the file 1-pack_web_static.py) that distributes an
+    archive to your web servers, using the function do_deploy"""
+
+from fabric.operations import local
+from datetime import datetime
+
+
+def do_pack(archive_path):
+    """Deploy archive"""
+    try:
+        local("mkdir -p versions")
+        l = local("tar -cvzf versions/web_static_{}.tgz web_static/"
+                  .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+        return l
+    except Exception:
+        return None
