5f4333b567a06d2937ec7e7987521255e681d6fb
snehadasa <sneha.dasasneha@gmail.com>
2019-08-19 17:32:32 +0000

    updating task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 
-
+apt-get update
+apt-get -y install nginxi
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web-static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+service nginx restart
+exit 0
