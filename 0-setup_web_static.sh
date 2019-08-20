357a5e2cc69e1169ff28210376d459eeda50cbad
snehadasa <sneha.dasasneha@gmail.com>
2019-08-20 02:36:14 +0000

    update 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@
 apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-mkdir -p /data/web-static/shared/
+mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "<html>
   <head>
@@ -15,6 +15,6 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sed -i "/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current;}" /etc/nginx/sites-available/default
 service nginx restart
 exit 0
