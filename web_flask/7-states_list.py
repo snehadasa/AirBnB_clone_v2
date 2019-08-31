cd7f328ef095d994ea8d96d1f3d2bd173e59fe8f
snehadasa <sneha.dasasneha@gmail.com>
2019-08-31 02:36:27 +0000

    task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """render template 7-number.html"""
+    return render_template('7-states_list.html', states=storage.all('State').values())
+
+@app.teardown_appcontext
+def app_teardown(self):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run()
