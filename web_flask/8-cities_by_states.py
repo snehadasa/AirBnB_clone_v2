40e675cd1e47fc5e79ef5ba45659db165e4f3cca
snehadasa <sneha.dasasneha@gmail.com>
2019-09-03 17:20:38 +0000

    pep8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,9 @@ app.url_map.strict_slashes = False
 @app.route('/cities_by_states')
 def cities_by_state():
     """render template 8-cities_by_states.html"""
-    return render_template('8-cities_by_states.html', states=storage.all('State').values())
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
 
 @app.teardown_appcontext
 def app_teardown(self):
